a27e9dac2a393bc49295355939c12cbe12f4c42e
Nhi Nguyen <n01593039@humber.ca>
2024-03-19 03:19:46 -0400

    about, menu, contact

---
+++ about.php
@@ -0,0 +1,35 @@
+<?php
+
+include('includes/database.php');
+include('includes/config.php');
+include('includes/functions.php');
+
+?>
+<!doctype html>
+<html>
+
+
+<?php
+    include('reusable/head.php')
+?>
+
+    <main>
+        <div class="container-fluid">
+            <div class="container my-5">
+                <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
+                <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
+                        <h2 class="mb-5">ABOUT US: Toronto Galery Guide</h2>
+                        <h6>We established in 2024, a website for you to find useful information for your destinations.</h6> 
+                        <h6>We also provide honest reviews and look forward to receiving your contributions to improve our website.</h6>
+                </div>
+                <div class="col-lg-4 offset-lg-1 p-0 overflow-hidden shadow-lg">
+                    <img class="rounded-lg-3" src="admin/imgs/museum-7409275_1280.jpg" alt="" width="720">
+                </div>
+            </div>
+        
+        </div>
+
+    </main>
+  </body>
+
+</html>
